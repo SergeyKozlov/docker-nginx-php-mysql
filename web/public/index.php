adb93ac5524a027b861a543b6d21674f83b9674e
Sergey Kozlov <mne@sergeykozlov.ru>
2022-12-01 22:00:26 +0300

    from 01122022-2

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,35 @@
 <?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sergey
+ * Date: 14.08.17
+ * Time: 10:00
+ */
 
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+//echo "test";
+//exit;
 
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
-</html>
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/nad/Model.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/nad/view/View.php');
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/nad/Controller.php');
+
+//include_once($_SERVER['DOCUMENT_ROOT'] . '/nad/index.php');
+//include_once($_SERVER['DOCUMENT_ROOT'] . '/pas/html/index.php');
+//include_once($_SERVER['DOCUMENT_ROOT'] . '/sendmail/sendmail.php');
+
+/*spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});*/
+
+//$welcome = new NAD();
+//$HTML = new HTML();
+//$sendmail = new sendmail();
+
+error_reporting(0); // Turn off error reporting
+//error_reporting(E_ALL); // Report all errors
+
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+
+$view->htmlIndex();
